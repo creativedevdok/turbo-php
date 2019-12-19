b126c5badc2a8de635c6f1c8b4f49ee14c87b5f9
“creativedevdok” <daniel.okenka@marinerescuetechnologies.com>
2019-12-19 09:26:56 -0500

    class car updated

--- class_car2.php
+++ class_car2.php
@@ -15,24 +15,29 @@ class Car{
 }
     
     if(class_exists("Car")) {
-        echo "it exist";
+        echo "it exist " . "<br>";
     } else  {
         echo "no";
     }
     
         if(method_exists("Car", "MoveWheels")) {
-        echo "it exist";
+        echo "Method exist"  . "<br>";
     } else  {
         echo "no";
     }
     
     
-
-$newCar = Car();
+////what i thought
+//$newCar = Car();
 //Car("MoveWheels");
 
+//bmw is an object which is an object of class Car
+$bmw = new Car();
+$mercedes_benz = new Car();
 
-
+//calling method of Car class
+$bmw->MoveWheels();
+$mercedes_benz->MoveWheels();
 
 
 
--- class_car3.php
+++ class_car3.php
@@ -2,10 +2,21 @@
 
 class Car{ 
     
+    var $wheels = 4;
+    var $hood = 1;
+    var $engine = 1;
+    var $doors = 4;
+    
     function MoveWheels(){
         echo "Wheels in motion" . "<br>";
+        //this references car class
+        $this->wheels = 3;
     }
     
+        function CreateDoors(){
+        $this->doors = 4;
+            echo "doors are now set to " . $this->doors . "<br>";
+    }
     
     
     
@@ -33,13 +44,19 @@ class Car{
 
 //bmw is an object which is an object of class Car
 $bmw = new Car();
-$mercedes_benz = new Car();
+//no $dollarsign for variable wheels in class Cars
+echo $bmw->wheels . "<br>";
+//override default wheels
+$bmw->wheels = 8;
+echo "wheels are now set to " . $bmw->wheels . "<br>";
 
-//calling method of Car class
 $bmw->MoveWheels();
-$mercedes_benz->MoveWheels();
+echo "wheels are now set to " . $bmw->wheels . "<br>";
 
+$truck = new Car();
+echo $truck->wheels = 10 . "<br>";
 
+echo "doors are now set to " . $truck->doors = 6;
 
 
 ?>  
